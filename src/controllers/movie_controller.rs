7610eda0a7a0dc7c0554ad649d0d4be250ee578a
Stepan Sendun <Stepan_Sendun@epam.com>
2023-11-25 13:45:55 +0200

    return list of movies from database

--- src/controllers/movie_controller.rs
+++ src/controllers/movie_controller.rs
@@ -1,7 +1,33 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{error, get, web, HttpResponse, Responder};
+use mongodb::{options::FindOptions, Client, Collection};
+
+use crate::{clients::mongo_client::MongoConfig, movie::Movie};
+use futures::stream::StreamExt;
 
 #[get("/movies")]
-async fn get_movies() -> impl Responder {
-    HttpResponse::Ok().body("Hello, world !!!")
-}
+async fn get_movies(client: web::Data<Client>) -> actix_web::Result<impl Responder> {
+    let config = MongoConfig::build().map_err(error::ErrorInternalServerError)?;
+    let movies_collection: Collection<Movie> = client
+        .database(&config.database)
+        .collection(&config.movies_collection);
 
+    let find_options = FindOptions::builder()
+        .sort(mongodb::bson::doc! { "id": 1 })
+        .build();
+
+    let result = movies_collection
+        .find(None, find_options)
+        .await
+        .map_err(error::ErrorInternalServerError)?
+        .collect::<Vec<_>>()
+        .await;
+
+    // todo add pagination
+
+    let result: Result<Vec<Movie>, _> = result.into_iter().collect();
+    let result = result.map_err(error::ErrorInternalServerError)?;
+
+    println!("Found {} movies", result.len());
+
+    Ok(HttpResponse::Ok().json(result))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-use actix_web::{middleware::Logger, App, HttpServer};
-
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use anyhow::Result;
+use api::clients::mongo_client::mongo_connect;
 use api::controllers::movie_controller::get_movies;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<()> {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
     log::info!("Starting server at http://localhost:3000");
 
-    HttpServer::new(|| App::new().service(get_movies).wrap(Logger::default()))
-        .bind("localhost:3000")?
-        .run()
-        .await
+    let client = mongo_connect().await?;
+
+    HttpServer::new(move || {
+        App::new()
+            // todo find a way to remove clone
+            .app_data(web::Data::new(client.clone()))
+            .service(get_movies)
+            .wrap(Logger::default())
+    })
+    .bind("localhost:3000")?
+    .run()
+    .await
+    .map_err(anyhow::Error::from)
 }
