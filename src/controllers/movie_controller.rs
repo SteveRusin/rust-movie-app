702471709ab309775f545e83314bcf80fe7b8bee
Stepan Sendun <Stepan_Sendun@epam.com>
2023-12-02 11:53:07 +0200

    add pagination

--- src/controllers/movie_controller.rs
+++ src/controllers/movie_controller.rs
@@ -1,11 +1,34 @@
 use actix_web::{error, get, web, HttpResponse, Responder};
 use mongodb::{options::FindOptions, Client, Collection};
+use serde::{Deserialize, Serialize};
 
 use crate::{clients::mongo_client::MongoConfig, movie::Movie};
 use futures::stream::StreamExt;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct MoviesReponse {
+    movies: Vec<Movie>,
+    total_count: u64,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+struct MovieParams {
+    limit: Option<i64>,
+    skip: Option<u64>,
+}
+
+impl MovieParams {
+    fn get_limit(&self) -> i64 {
+        self.limit.unwrap_or(10)
+    }
+
+    fn get_skip(&self) -> u64 {
+        self.skip.unwrap_or(0)
+    }
+}
+
 #[get("/movies")]
-async fn get_movies(client: web::Data<Client>) -> actix_web::Result<impl Responder> {
+async fn get_movies(client: web::Data<Client>, params: web::Query<MovieParams>) -> actix_web::Result<impl Responder> {
     let config = MongoConfig::build().map_err(error::ErrorInternalServerError)?;
     let movies_collection: Collection<Movie> = client
         .database(&config.database)
@@ -13,6 +36,8 @@ async fn get_movies(client: web::Data<Client>) -> actix_web::Result<impl Respond
 
     let find_options = FindOptions::builder()
         .sort(mongodb::bson::doc! { "id": 1 })
+        .limit(params.get_limit())
+        .skip(params.get_skip())
         .build();
 
     let result = movies_collection
@@ -22,12 +47,17 @@ async fn get_movies(client: web::Data<Client>) -> actix_web::Result<impl Respond
         .collect::<Vec<_>>()
         .await;
 
-    // todo add pagination
+    let total_count = movies_collection
+        .count_documents(None, None)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
 
     let result: Result<Vec<Movie>, _> = result.into_iter().collect();
     let result = result.map_err(error::ErrorInternalServerError)?;
-
-    println!("Found {} movies", result.len());
+    let result = MoviesReponse {
+        movies: result,
+        total_count,
+    };
 
     Ok(HttpResponse::Ok().json(result))
 }
