1e2b73d5c5b394d08474623f4dee43b33e8ba516
Stepan Sendun <Stepan_Sendun@epam.com>
2023-12-02 12:14:55 +0200

    request db in parallerl

--- src/controllers/movie_controller.rs
+++ src/controllers/movie_controller.rs
@@ -1,8 +1,10 @@
 use actix_web::{error, get, web, HttpResponse, Responder};
+use anyhow::Result;
 use mongodb::{options::FindOptions, Client, Collection};
 use serde::{Deserialize, Serialize};
 
 use crate::{clients::mongo_client::MongoConfig, movie::Movie};
+use futures::join;
 use futures::stream::StreamExt;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -28,7 +30,10 @@ impl MovieParams {
 }
 
 #[get("/movies")]
-async fn get_movies(client: web::Data<Client>, params: web::Query<MovieParams>) -> actix_web::Result<impl Responder> {
+async fn get_movies(
+    client: web::Data<Client>,
+    params: web::Query<MovieParams>,
+) -> actix_web::Result<impl Responder> {
     let config = MongoConfig::build().map_err(error::ErrorInternalServerError)?;
     let movies_collection: Collection<Movie> = client
         .database(&config.database)
@@ -40,24 +45,38 @@ async fn get_movies(client: web::Data<Client>, params: web::Query<MovieParams>) 
         .skip(params.get_skip())
         .build();
 
-    let result = movies_collection
-        .find(None, find_options)
-        .await
-        .map_err(error::ErrorInternalServerError)?
-        .collect::<Vec<_>>()
-        .await;
+    let (result, total_count) = join!(
+        get_movies_collection(&movies_collection, find_options),
+        get_total_count(&movies_collection)
+    );
 
-    let total_count = movies_collection
-        .count_documents(None, None)
-        .await
-        .map_err(error::ErrorInternalServerError)?;
-
-    let result: Result<Vec<Movie>, _> = result.into_iter().collect();
     let result = result.map_err(error::ErrorInternalServerError)?;
+    let total_count = total_count.map_err(error::ErrorInternalServerError)?;
+
     let result = MoviesReponse {
         movies: result,
         total_count,
     };
 
     Ok(HttpResponse::Ok().json(result))
+}
+
+async fn get_total_count(movies_collection: &Collection<Movie>) -> Result<u64> {
+    return Ok(movies_collection
+        .count_documents(None, None)
+        .await
+        .map_err(|e| anyhow::anyhow!("Error getting total count: {}", e))?);
+}
+
+async fn get_movies_collection(
+    movies_collection: &Collection<Movie>,
+    find_options: FindOptions,
+) -> Result<Vec<Movie>> {
+    return Ok(movies_collection
+        .find(None, find_options)
+        .await?
+        .collect::<Vec<_>>()
+        .await
+        .into_iter()
+        .collect::<Result<Vec<_>, _>>()?);
 }
